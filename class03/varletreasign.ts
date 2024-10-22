45ce4e9ab3f207bd52d20b5e9cfa571443320ad2
hammad_ul_haq <hammadulhaq32@gmail.com>
2024-10-23 00:04:04 +0500

    Create varletreasign.ts

---
+++ class03/varletreasign.ts
@@ -0,0 +1,11 @@
+let sunny="biryani lay ana";
+
+sunny="nihari lay ana";
+
+sunny="alo qeema"
+
+// This code will produce potato minced meat when it runs.
+
+console.log(sunny)
+
+console.log(sunny)
