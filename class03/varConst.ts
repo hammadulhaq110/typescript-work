ad55fc82b6b9437d9d224ef4dbff6fbb3d558af9
hammad_ul_haq <hammadulhaq32@gmail.com>
2024-10-22 23:53:42 +0500

    Create varConst.ts

---
+++ class03/varConst.ts
@@ -0,0 +1,11 @@
+// variable with const keyword
+
+const myName = "Hammad"; // Declare
+
+console.log(myName);
+
+// myName = "Javed"; // try to re-assign it will give error
+
+console.log(myName);
+
+// const myName = "Javed ahmed"; // try to re-declare it will give error
