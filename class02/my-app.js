10b82a18ad6b8650b8d1f275cf54d9db915b14b9
hammad_ul_haq <hammadulhaq32@gmail.com>
2024-10-22 23:47:52 +0500

    Create my-app.js

---
+++ class02/my-app.js
@@ -0,0 +1,8 @@
+var myName = "hammad";
+console.log(myName);
+console.log(myName);
+console.log(myName);
+//let favouritecolor="green";
+//console.log(favouritecolor)
+//favouritecolor="yellow";
+//console.log(favouritecolor)
