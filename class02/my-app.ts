0f980063c990be264445a0eebe02870067dfb8a3
hammad_ul_haq <hammadulhaq32@gmail.com>
2024-10-22 23:47:14 +0500

    Create my-app.ts

---
+++ class02/my-app.ts
@@ -0,0 +1,10 @@
+let myName="hammad"
+
+console.log(myName)
+console.log(myName)
+console.log(myName)
+
+//let favouritecolor="green";
+//console.log(favouritecolor)
+//favouritecolor="yellow";
+//console.log(favouritecolor)
