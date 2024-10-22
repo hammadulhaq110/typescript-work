3c96c9d5818bd364f62beccd0ff49692f844a409
hammad_ul_haq <hammadulhaq32@gmail.com>
2024-10-23 01:42:02 +0500

    Create app.ts

---
+++ class12/todo/app.ts
@@ -0,0 +1,8 @@
+import { addTask, showTasks } from "./todo.js";
+
+addTask("Fix Abbu's laptop");
+addTask("Finish homework before dinner");
+addTask("Prepare Biryani");
+addTask("Convince Ammi for an shaadi");
+
+showTasks();
